45a88c68fec8689062ff67b00eb60fd7ea1f8669
vasipallikarthikreddy207-maker <vasipallikarthikreddy207@gmail.com>
2026-03-18 09:13:04 +0530

    Refactor main method and add loop for output
    Refactor main method to remove command-line arguments and add a loop.

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 import java.io.*;
 
-public class Main {
-    public static void main(String[] args) {
+void main() {
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    IO.println(String.format("Hello and welcome!"));
 
-        if(args.length > 0){
-            String name = args[0];
-            System.out.println("Hello " + name);
-        } else {
-            System.out.println("Hello World");
-        }
+    for (int i = 1; i <= 5; i++) {
+        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        IO.println("i = " + i);
 
     }
 }
